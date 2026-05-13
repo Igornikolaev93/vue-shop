91303291ad115df04d7f0fd2c5d66d5a4eb5de8b
Игорь Николаев <goscha.nikolae2015@yandex.ru>
2026-05-13 19:38:36 +0300

    at

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,22 @@ export default defineConfig({
         }
     },
     base: './',
+    build: {
+        rollupOptions: {
+            output: {
+                // Переименовываем файлы, начинающиеся с _
+                chunkFileNames: (chunkInfo) => {
+                    const name = chunkInfo.name;
+                    if (name.startsWith('_')) {
+                        return `assets/vendor-${name.slice(1)}.js`;
+                    }
+                    return `assets/[name]-[hash].js`;
+                },
+                entryFileNames: 'assets/[name]-[hash].js',
+                assetFileNames: 'assets/[name]-[hash].[ext]'
+            }
+        }
+    },
     server: {
         port: 3000,
         open: true
